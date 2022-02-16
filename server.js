26223844421aaa3d8d86dec2bc7c5224b0618a1e
Gleb932 <gleb.g932@gmail.com>
2022-02-17 01:19:48 +0200

    Port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var PORT = 3000;
+const PORT = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
     res.status(200).send('Hello world');
